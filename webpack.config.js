07c5c9bc54ba39d35fdd8e7e484cbb8d3730ae9d
Andreas Rau <raua@CT.local>
2018-10-19 13:47:59 +0200

    Setup HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './client/src/app.js',
+    mode: 'development',
     output: {
         filename: 'app.js',
         path: __dirname + '/client/dist'
@@ -12,10 +15,15 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-react','@babel/preset-env']
                     }
                 }
             }
         ]
-    }
+    },
+    plugins : [new HtmlWebpackPlugin(
+        {
+            template: __dirname + '/client/src/index.html'
+        }
+    )]
 }
